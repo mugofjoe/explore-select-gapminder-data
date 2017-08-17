cd7b355f9c77cd691a53e9ee1643fe00f4d44d42
Joel Dazo <joel.d.dazo@gmail.com>
2017-08-16 22:27:41 -0400

    Added python programming project.

---
+++ Gapminder1D/Gapminder1D/Gapminder1D.py
@@ -0,0 +1,14 @@
+'''
+Udacity Questions on Gapminder data
+1. How has employment in the U.S. (or another country) varied over time?
+2. In the most recent year we have data on, what are the highest and lowest employment levels?
+    - which countries have them?
+    - where is the U.S. on the spectrum
+
+Same questions for other variables
+How do these variables relate to each other?
+Are there consistent trends across countries?
+  - have there been any global recessions?
+  - can we find them in this data?
+
+'''
