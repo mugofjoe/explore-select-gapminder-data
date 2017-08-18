7c7c0bbcbf06d6863692db7fefc270bc060454ef
Joel Dazo <joel.d.dazo@gmail.com>
2017-08-17 23:36:18 -0400

    Added pandas data load.

--- Gapminder1D/Gapminder1D/Gapminder1D.py
+++ Gapminder1D/Gapminder1D/Gapminder1D.py
@@ -12,3 +12,18 @@ Are there consistent trends across countries?
   - can we find them in this data?
 
 '''
+
+# 
+# Load data using pandas
+#
+
+import pandas as pd
+daily_engagement_full = pd.read_csv('..\\..\\data\\daily_engagement_full.csv')
+print("Total accounts daily engagement: {}".format(len(daily_engagement_full)))
+
+# Use pandas unique function to 
+# get unique engagement accounts 
+
+count_unique_engagement = len(daily_engagement_full['acct'].unique())
+print("Unique accounts daily engagement: {}".format(count_unique_engagement))
+
