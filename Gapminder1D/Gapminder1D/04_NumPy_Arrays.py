581233a97be3461b0cc5de6e672e82169737d340
Joel Dazo <joel.d.dazo@gmail.com>
2017-08-21 00:09:43 -0400

    Added code for vector additions.

--- Gapminder1D/Gapminder1D/04_NumPy_Arrays.py
+++ Gapminder1D/Gapminder1D/04_NumPy_Arrays.py
@@ -77,7 +77,7 @@ print np.array(['AL', 'AK', 'AZ', 'AR', 'CA']).dtype
 for country in countries:
     print 'Examining country {}'.format(country)
 
-for i in range(len(countries)):
+for i in xrange(len(countries)):
     country = countries[i]
     country_employment = employment[i]
     print 'Country {} has employment {}'.format(country,
@@ -91,15 +91,82 @@ print employment.std()
 print employment.max()
 print employment.sum()
 
-def max_employment(countries, employment):
-    '''
-    Fill in this function to return the name of the country
-    with the highest employment in the given employment
-    data, and the employment in that country.
-    '''
-    max_value = employment.max()
-    max_country = countries[max(np.where(employment == max_value)[0])]
 
-    return (max_country, max_value)
+'''
+Below is my own answer to the assignment.
+'''
+
+# def max_employment(countries, employment):
+#    '''
+#    Fill in this function to return the name of the country
+#    with the highest employment in the given employment
+#    data, and the employment in that country.
+#    '''
+#    max_country = None
+#    max_employment = 0
+
+#    max_value = employment.max()
+#    max_country = countries[max(np.where(employment == max_value)[0])]
+
+#    return (max_country, max_value)
+
+def max_employment(countries, employment):
+    max_country = None
+    max_employment = 0
+
+    # loop through each element of the two 
+    # numPy arrays. Each array has the same length
+    for i in range(len(countries)):
+        country = countries[i]
+        country_empoloyment = employment[i]
+
+        # check if the current employment is greater
+        # than the employment we've seen so far
+        if country_employment > max_employment:
+            max_country = country
+            max_employment = country_employment
+
+    return (max_country, max_employment)
+
+'''
+Another way to code max_employment
+'''
+def max_employment2(countries, employment):
+    i = employment.argmax()
+    return (countries[i], employment[i])
+
+
+'''
+Arithmetic operations between two NumPy arrays
+'''
+
+a = np.array([1,2,3,4])
+b = np.array([1,2,1,2])
+
+print a + b
+print a - b
+print a * b
+print a / b
+print a ** b
+
+'''
+Arithmetic operations between a NumPy array and a single number
+'''
+
+a = np.array([1, 2, 3, 4])
+b = 2
+    
+print a + b
+print a - b
+print a * b
+print a / b
+print a ** b
+
+
+
+
+
+
+
 
 
